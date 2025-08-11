db9a427efdff917ce9d737aa7d994e41a59db0f3
Artem <amorlov.sev@gmail.com>
2025-08-11 16:09:06 +0300

    hexlet checkstyle javadoc

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -12,6 +12,33 @@ public abstract class BaseSchema<T> {
         checks.put(name, validate);
     }
 
+    /**
+     * Sets the schema to require non-null values for validation.
+     * <p>
+     * When activated, this validation will fail for any null values passed to {@link #isValid(Object)}.
+     * The requirement is stored as a predicate check with the key "required".
+     * </p>
+     *
+     * <p><b>Note for implementers:</b>
+     * <ul>
+     *   <li>Subclasses should call {@code super.required()} when overriding this method</li>
+     *   <li>Subclasses may add additional requiredness checks (like empty string validation)</li>
+     *   <li>Multiple calls will overwrite the previous required check</li>
+     * </ul>
+     * </p>
+     *
+     * @return the current schema instance (this) to enable method chaining
+     *
+     * @see #isValid(Object) For the validation execution
+     * @see StringSchema#required() For an example of extended required validation
+     * @see NumberSchema#required() For number-specific required validation
+     *
+     * @example
+     * // Basic usage
+     * BaseSchema<String> schema = new StringSchema().required();
+     * schema.isValid(null);  // returns false
+     * schema.isValid("");    // returns true (unless overridden by subclass)
+     */
     public BaseSchema<T> required() {
         this.required = true;
         addCheck("required", value -> value != null);
